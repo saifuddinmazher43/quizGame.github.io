ce27924e97706cf910c260688856941a1dd6bf7f
Saifuddin <139206954+saifuddinmazher43@users.noreply.github.com>
2023-07-15 01:35:42 +0500

    Add files via upload

---
+++ question.js
@@ -0,0 +1,111 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which flower is national flower of pakistan?",
+    answer: "Jasmine",
+    options: [
+      "Jasmine",
+      "Rose",
+      "Sunflower",
+      "None of these"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How many rivers are there in punjab?",
+    answer: "5",
+    options: [
+      "3",
+      "7",
+      "5",
+      "1"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which country and its territory covers the most time zones?",
+    answer: "France",
+    options: [
+      "France",
+      "Spain",
+      "Germany",
+      "Greece"
+    ]
+  },
+    {
+    numb: 4,
+    question: "When was the Famous children's television show \"Ainak Wala Jinn Broadcasted\" by PTV?",
+    answer: "1993",
+    options: [
+      "1990",
+      "1991",
+      "1992",
+      "1993"
+    ]
+  },
+    {
+    numb: 5,
+    question: "World polio day observed every year on?",
+    answer: "Octuber 24",
+    options: [
+      "Octuber 23",
+      "Octuber 24",
+      "November 23",
+      "November 24"
+    ]
+  },
+  {
+    numb: 6,
+    question: "The Smallest Continent (by Area) in the World is?",
+    answer: "Australia",
+    options: [
+      "South America",
+      "Africa",
+      "Australia",
+      "Antartica"
+    ]
+  },    {
+    numb: 7,
+    question: "The Second largest city of Pakistan is?",
+    answer: "Lahore",
+    options: [
+      "Faislabad",
+      "Lahore",
+      "Karachi",
+      "Rawalpindi"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which country is called \"Land Of Thousand Islands\"?",
+    answer: "Indonesia",
+    options: [
+      "Maldives",
+      "Philippines",
+      "Indonesia",
+      "Malaysia"
+    ]
+  },
+  {
+    numb: 9,
+    question: "22 April is observed internationally as?",
+    answer: "Earth Day",
+    options: [
+      "Labour Day",
+      "Enviroment Day",
+      "Health Day",
+      "Earth Day"
+    ]
+  },
+  {
+    numb: 10,
+    question: "How many districts are in sindh?",
+    answer: "30",
+    options: [
+      "27",
+      "28",
+      "29",
+      "30"
+    ]
+  },
+];
